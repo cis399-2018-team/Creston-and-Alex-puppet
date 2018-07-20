e29852b7cc8f8c874de68370b3984bcc92414838
Creston Wilson <crestonw@github.com>
2018-07-20 14:21:42 -0700

    Rename

---
+++ modules/apache/manifests/init.pp
@@ -0,0 +1,26 @@
+class httpd {
+
+	# install
+	package{'apache2':
+		ensure => latest,
+	}
+	
+	#webpage resource
+	file{'index.html':
+		ensure  => file,
+		path    => '/var/www/html/index.html',
+		content => '<h1>Hello, World</h1>',
+		require => Package['apache2'],
+		owner   => root,
+		mode    => '0600',
+		notify  => Service['httpd'],
+	}
+
+	#keeps service running
+	service{'httpd':
+		ensure     => running,
+		enable     => true,
+		hasrestart => true, 
+		require    => Package['apache2'],
+	}
+}
