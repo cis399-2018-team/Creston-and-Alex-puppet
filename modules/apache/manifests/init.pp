162890e86aaa33256c7bf19802449d2b5c61f2df
Creston Wilson <crestonw@github.com>
2018-07-23 08:12:12 -0700

    Update

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -5,6 +5,20 @@ class apache {
 		ensure => latest,
 	}
 	
+	file{'/etc/apache2/apache2.conf':
+		ensure  => present,
+		source  => ["puppet:///modules/apache/$hostname/apache2.conf",
+			"puppet:///modules/apache/apache2.conf"],
+		require => Package['apache2'],
+	}
+
+	file{'/var/www/html':
+		ensure  => directory,
+		recurse => true
+		source  => ["puppet:///modules/apache/files/"],
+		require => Package['apache2'],
+	}	
+
 	#webpage resource
 	file{'index.html':
 		ensure  => file,
