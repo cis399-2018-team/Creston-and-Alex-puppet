d0b7916fce35a48dcb8c0a7a8bf70c707fb00c8d
root <root@ip-10-0-6-195.us-west-2.compute.internal>
2018-07-23 22:56:28 +0000

    testing syntax fix

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -12,10 +12,10 @@ class apache {
 	}	
 
 	#webpage resource
-	file{'/var/www/html/index.html':
-		ensure  => file,
-		source  => ["puppet:///modules/apache/index.html",],
-		recurse => true,
+	file{'/var/www/html/':
+		ensure  => directory,
+                recurse => true,
+		source  => ["puppet:///modules/apache/html",],
 		require => Package['apache2'],
 		notify  => Service['apache2'],
 	}
@@ -24,9 +24,7 @@ class apache {
 	service{'apache2':
 		ensure     => running,
 		enable     => true,
-		hasstatus  => true,
 		hasrestart => true, 
-		require    => [Package['apache2'], File["/etc/apache2/apache2.conf"], File["/var/www/html/index.html"]],
-		subscribe  => File["/etc/apache2/apache2.conf"],
+		require    => Package['apache2'],
 	}
 }
