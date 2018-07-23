bb5832f7780eb4b0139ae40769f1fd5fce8f1269
root <root@ip-10-0-6-195.us-west-2.compute.internal>
2018-07-23 20:26:27 +0000

    testing changes to index

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -7,14 +7,15 @@ class apache {
 	
 	file{'/etc/apache2/apache2.conf':
 		ensure  => present,
-		source  => ["puppet:///modules/apache/files/apache2.conf"],
+		source  => ["puppet:///modules/apache/apache2.conf",],
 		require => Package['apache2'],
 	}	
 
 	#webpage resource
-	file{'/var/www/html':
-		ensure  => directory,
-		source  => ["puppet:///modules/apache/files/html",],
+	file{'/var/www/html/index.html':
+		ensure  => file,
+		path    => '/var/www/html/index.html',
+		source  => ["puppet:///modules/apache/index.html",],
 		require => Package['apache2'],
 		notify  => Service['apache2'],
 	}
