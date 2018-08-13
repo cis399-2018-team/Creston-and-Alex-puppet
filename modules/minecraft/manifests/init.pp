4923d582f63a7946c7fd3283fe6411133de36c10
Creston Wilson <35243873+crestonw@users.noreply.github.com>
2018-08-13 16:47:55 -0700

    Update init.pp

--- modules/minecraft/manifests/init.pp
+++ modules/minecraft/manifests/init.pp
@@ -3,7 +3,7 @@ class minecraft {
 
 	file{'/server.jar':
 		owner   => root,
-		ensure  =>  file,
+		ensure  =>  [file,present],
 		mode    => '0750',
 		source  => ["puppet:///modules/minecraft/server.jar",],
 	}
